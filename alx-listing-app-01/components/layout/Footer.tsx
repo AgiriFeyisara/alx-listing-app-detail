3ccec46492c4fe456a3e043a56b6917b09791cfd
Feyisara Agiri <mohrenny8@gmail.com>
2025-11-27 23:45:56 +0100

    Added the property detail components

--- alx-listing-app-01/components/layout/Footer.tsx
+++
@@ -1,8 +0,0 @@
-const Footer = () =>{
-    return(
-     <footer className="w-full text-center py-6 bg-gray-100 mt-10">
-        <p className="text-sm">&copy; 2025 Listing App. All rights reserved.</p>
-     </footer>
-    )
-}
-export default Footer
